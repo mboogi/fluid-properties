1c3b3dd51f771120b9db1828836264a21208576a
MB <bog_milos@protonmail.com>
2023-05-27 13:54:54 +0200

    backup

--- CoolPropService.py
+++ CoolPropService.py
@@ -7,4 +7,5 @@ def get_available_fluids() -> str:
 
 def get_numeric_property(output, name1, prop1, name2, prop2, fluid_name: str):
     result = PropsSI(output, name1, prop1, name2, prop2, fluid_name)
+    # print("output: "+str(output)+" name1 "+str(name1)+" name2 "+str(name2)+" fluid "+str(fluid_name)+" prop1 "+str(prop1)+" prop2 "+str(prop2)+" res "+str(result))
     return result
